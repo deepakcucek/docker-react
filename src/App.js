1b7d9153c068ab79f7f8d2cd3e1801f93b2f255e
deepakcucek <deepakcucek07@gmail.com>
2023-09-25 14:04:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          new edit ok. sure
+          new edit ok. go for it
         </p>
         <a
           className="App-link"
